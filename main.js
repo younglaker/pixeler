67aa9503b2105de9cc647687dc7080d920420eb0
laker <younglaker8@gmail.com>
2015-10-05 22:18:54 +0800

    给全局变量加前缀'g_'

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var circleData = []; //这个为画布的二维数组用来保存画布信息，初始化0为没有填充的，1为已填充的
+    var g_canvas = document.getElementById("canvas");
+    var g_ctx = g_canvas.getContext("2d");
+    var g_circle_data = []; //这个为画布的二维数组用来保存画布信息，初始化0为没有填充的，1为已填充的
     var grid_width = 40; // 画布格子宽度
-    var circleColor = "#fff"; // 默认颜色为白色
-    var curr_color_box = O("#color_cnt_white");
-    var curr_color_id = O("#color_cnt_white").getAttr("id");
-    // var row = 9, col = 11; // row:行数，col:列数
-    var col = $(".siser_col").val(), 
-        row = $(".siser_row").val(); // col:列数/长，row:行数/宽0
+    var g_circle_color = "#fff"; // 默认颜色为白色
+    var g_curr_color_box = O("#color_cnt_white");
+    var g_curr_color_id = O("#color_cnt_white").getAttr("id");
+    // var g_row = 9, g_col = 11; // g_row:行数，g_col:列数
+    var g_col = $(".siser_col").val(), 
+        g_row = $(".siser_row").val(); // g_col:列数/长，g_row:行数/宽0
 
     /*
      *  绘制画布
      *  页面加载完毕调用函数，初始化画布
      */
     function drawRect() {  
-        var x_width = col * grid_width,
-            y_width = row * grid_width;
+        var x_width = g_col * grid_width,
+            y_width = g_row * grid_width;
 
         // i从20开始，是为了留位置显示坐标
         for (var i = 20; i <= y_width + 20; i += grid_width) {  //绘制行
-            ctx.beginPath();
-            ctx.fillStyle = "#000";
-            ctx.fillText((i - 20 ) / grid_width, 0, i - 20);
-            ctx.moveTo(20, i);
-            ctx.lineTo(x_width + 20, i);
-            ctx.closePath();
-            ctx.stroke();
+            g_ctx.beginPath();
+            g_ctx.fillStyle = "#000";
+            g_ctx.fillText((i - 20 ) / grid_width, 0, i - 20);
+            g_ctx.moveTo(20, i);
+            g_ctx.lineTo(x_width + 20, i);
+            g_ctx.closePath();
+            g_ctx.stroke();
         }
 
         for (var i = 20; i <= x_width + 20; i += grid_width) {  //绘制列
-            ctx.beginPath();
-            ctx.fillText((i - 20 ) / grid_width, i - 20, 10);
-            ctx.moveTo(i, 20);
-            ctx.lineTo(i, y_width + 20);
-            ctx.closePath();
-            ctx.stroke();
+            g_ctx.beginPath();
+            g_ctx.fillText((i - 20 ) / grid_width, i - 20, 10);
+            g_ctx.moveTo(i, 20);
+            g_ctx.lineTo(i, y_width + 20);
+            g_ctx.closePath();
+            g_ctx.stroke();
         }
 
 
         // 设置与画布对应的二维数组
-        circleData = [];
-        for (var x = 0; x < col; x++) {
-            circleData[x] = [];
-            for (var y = 0; y < row; y++) {
-                circleData[x][y] = 0;
+        g_circle_data = [];
+        for (var x = 0; x < g_col; x++) {
+            g_circle_data[x] = [];
+            for (var y = 0; y < g_row; y++) {
+                g_circle_data[x][y] = 0;
             }
         }
 
@@ -58,8 +58,8 @@
         var x = parseInt((e.offsetX-20) / 40);  //计算鼠标点击的区域，e.offsetX是鼠标点击处在元素内的位置。如果点击了（55，55），那么就是点击了（1，1）的位置
         var y = parseInt((e.offsetY-20) / 40);
 
-        if (x < col && y < row) {
-            if (circleData[x][y] != 0) { //判断该位置是否被下过了
+        if (x < g_col && y < g_row) {
+            if (g_circle_data[x][y] != 0) { //判断该位置是否被下过了
                 clearCircle(x, y);
                 return;
             }
@@ -76,17 +76,17 @@
         var count;
 
         if (x >= 0 && x < 15 && y >= 0 && y < 15) {
-            ctx.fillStyle = circleColor;
-            ctx.beginPath();
-            ctx.arc((x + 1) * 40, (y + 1) * 40, 15, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fill();
+            g_ctx.fillStyle = g_circle_color;
+            g_ctx.beginPath();
+            g_ctx.arc((x + 1) * 40, (y + 1) * 40, 15, 0, Math.PI * 2, true);
+            g_ctx.closePath();
+            g_ctx.fill();
             // 所记录的颜色计数标签id
-            circleData[x][y] = curr_color_id;
+            g_circle_data[x][y] = g_curr_color_id;
         }
         // 计数加1
-        count = parseInt(curr_color_box.html()) + 1;
-        curr_color_box.html(count);
+        count = parseInt(g_curr_color_box.html()) + 1;
+        g_curr_color_box.html(count);
     }
 
     /*
@@ -94,34 +94,34 @@
      */
     function clearCircle(x, y) {
         var count;
-        var del_color = O("#" + circleData[x][y]);
+        var del_color = O("#" + g_circle_data[x][y]);
         // 清除拼豆
-        ctx.fillStyle = "#ddd";
-        ctx.beginPath();
-        ctx.arc((x + 1) * 40, (y + 1) * 40, 16, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fill();
+        g_ctx.fillStyle = "#ddd";
+        g_ctx.beginPath();
+        g_ctx.arc((x + 1) * 40, (y + 1) * 40, 16, 0, Math.PI * 2, true);
+        g_ctx.closePath();
+        g_ctx.fill();
         // 计数减1
         count = parseInt(del_color.html()) - 1;
         del_color.html(count);
-        // 清除circleData所记录的颜色计数标签id
-        circleData[x][y] = 0;
+        // 清除g_circle_data所记录的颜色计数标签id
+        g_circle_data[x][y] = 0;
     }
 
     /*
      *  清除拼豆
      */
     function clearCanvas() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);   
+        g_ctx.clearRect(0, 0, g_canvas.width, g_canvas.height);   
     }
 
     /*
      *  获取新颜色
      */
     O(".colors").click(function(){
-        circleColor = Oct.rgbToHex(O(this).getCss("background-color"));
-        curr_color_box = O(this).parent().son(".color_count");
-        curr_color_id = curr_color_box.getAttr("id");
+        g_circle_color = Oct.rgbToHex(O(this).getCss("background-color"));
+        g_curr_color_box = O(this).parent().son(".color_count");
+        g_curr_color_id = g_curr_color_box.getAttr("id");
 
         var ds = this;
         // 清除原来含有.act的按钮
@@ -143,10 +143,10 @@
     O(".siser_ok").click(function(){
         var conf = confirm("更换画布会清空拼豆，是否确定？");
         if (conf == true) {
-            col = $(".siser_col").val();
-            row = $(".siser_row").val(); 
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawRect(col, row);
+            g_col = $(".siser_col").val();
+            g_row = $(".siser_row").val(); 
+            g_ctx.clearRect(0, 0, g_canvas.width, g_canvas.height);
+            drawRect(g_col, g_row);
         } else {
             return false;
         }
@@ -156,7 +156,7 @@
         var conf = confirm("是否确定清空？");
         if (conf == true) {
             clearCanvas();
-            drawRect(col, row);
+            drawRect(g_col, g_row);
         } else {
             return false;
         }
