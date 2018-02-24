b063acbbd458566d969cb7e2d43c4e13c7e135ac
younglaker <younglaker8@gmail.com>
2018-02-25 00:17:49 +0800

    add: gulp settings and new project stucture

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,55 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
   plumber = require('gulp-plumber'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  clean = require('gulp-clean'),
+  minifyCss = require('gulp-minify-css'),
   browserSync = require('browser-sync').create();
 
 
-// 编译less，其中plumber是防止出错崩溃的插件
+// 编译前清空
+gulp.task('clean', function() {
+  gulp.src(['dist/', 'public'])
+    .pipe(clean({ force: true }));
+});
+
+
+
+// 编译sass，其中plumber是防止出错崩溃的插件
 gulp.task('sass', function() {
   gulp.src('src/sass/*.sass')
     .pipe(plumber())
     .pipe(sass())
     .pipe(gulp.dest('dist/css'));
+});
+
+// 将所有css文件连接为一个文件并压缩，存到public/css
+gulp.task('concatCss', function() {
+  gulp.src(['dist/css/*.css'])
+    .pipe(concat('main.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/css'));
+});
+
+
+// 将所有js文件连接为一个文件并压缩，存到public/js
+gulp.task('concatJs', function() {
+  gulp.src(['dist/js/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
+});
+
+// 复制图片
+gulp.task('copy_img', function() {
+
+/*  gulp.src('public/img/')
+    .pipe(clean({ force: true }));*/
+
+  gulp.src('src/img/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('public/img/'));
 });
 
 
@@ -29,6 +69,7 @@ gulp.task('watch', function() {
 
   // 预处理 **表示各个层级
   gulp.watch('src/sass/**', ['sass']);
+  gulp.watch('src/img/**', ['copy_img']);
 
 
   // 自动刷新
